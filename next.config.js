721ed53413c922f605be6a55b77675beaf119aee
3Alan <1540703192@qq.com>
2021-11-03 18:06:42 +0800

    fix: Skipping validation of types

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  eslint: {
+  typescript: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
-    ignoreDuringBuilds: true
+    ignoreBuildErrors: true
   }
 };
